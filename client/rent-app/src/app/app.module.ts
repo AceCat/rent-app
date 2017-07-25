937c50ad2c5e6c7b95b38857cfb4a32c9875c068
AceCat <myers.benn@gmail.com>
2017-07-25 09:45:24 -0500

    You can draw a map now

--- client/rent-app/src/app/app.module.ts
+++ client/rent-app/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { FormsModule } from '@angular/forms';
 import { D3Service } from 'd3-ng2-service';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import * as topojson from '@types/topojson';
+
+// import * as topojson from '../../node_modules/@types/topojson';
 
 
 const routes: Routes = [
--- client/rent-app/src/app/map/map.component.ts
+++ client/rent-app/src/app/map/map.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
-// import {Http, Response} from '@angular/http';
+import {Http, Response} from '@angular/http';
 import { Router } from '@angular/router';
 import { D3Service, D3, Selection } from 'd3-ng2-service'; 
+declare let topojson: any;
 
 
 
@@ -12,31 +13,52 @@ import { D3Service, D3, Selection } from 'd3-ng2-service';
 })
 
 export class MapComponent implements OnInit {
-
 	private d3: D3; // <-- Define the private member which will hold the d3 reference
 	private parentNativeElement: any;
 
 
-  constructor(private router: Router, element: ElementRef, d3Service: D3Service) { // <-- pass the D3 Service into the constructor
+  constructor(private http: Http, private router: Router, element: ElementRef, d3Service: D3Service) { // <-- pass the D3 Service into the constructor
     this.d3 = d3Service.getD3(); // <-- obtain the d3 object from the D3 Service
     this.parentNativeElement = element.nativeElement;
   }
 
+
+
+
   ngOnInit() {
+  	this.makeMap()
   }
 
+makeMap () {
+	var width = 1200
+    var height = 1200;
 
-	// makeMap () {
-	// 	var context = this.d3.select("canvas").getContext("2d"),
-	//     path = this.d3.geoPath().context(context);
+var svg = this.d3.select( "body" )
+  .append( "svg" )
+  .attr( "width", width )
+  .attr( "height", height );
 
-	// 	this.d3.json("https://unpkg.com/us-atlas@1/us/10m.json", function(error, us) {
-	//   	if (error) throw error;
+var g = svg.append( "g" );
 
-	//   context.beginPath();
-	//   path(this.topojson.mesh(us));
-	//   context.stroke();
-	// 	});
-	// }
+var albersProjection = this.d3.geoAlbers()
+  .scale( 2000 )
+  .rotate( [71.057,0] )
+  .center( [0, 42.313] )
+  .translate( [width/2,height/2] );
+
+var geoPath = this.d3.geoPath()
+    .projection( albersProjection );
+this.http.get('https://raw.githubusercontent.com/jgoodall/us-maps/master/geojson/state.geo.json').subscribe(response => {
+console.log(response.json().features)
+g.selectAll( "path" )
+  .data(response.json().features)
+  .enter()
+  .append( "path" )
+  .attr( "fill", "#ccc" )
+  .attr( "d", geoPath )
+  		})
+	}
 
 }
+
+
